98b9006ba25a978da7a4e70ee882727fd3ecc9e3
SangMin Park <qkrtkdals268@naver.com>
2025-03-14 14:52:55 +0900

    3월14일 제출

---
+++ 3월12일 과제/index.js
@@ -0,0 +1,22 @@
+const body = document.body;
+
+let h1 = document.createElement("h1");
+h1.textContent = "자기소개페이지";
+body.appendChild(h1);
+
+let img = document.createElement("img");
+img.src = "내사진.jpg";
+img.alt = "프로필 이미지";
+img.style.height = "400px";
+img.style.width = "300px";
+img.style.borderRadius = "50%";
+body.appendChild(img);
+
+let p1 = document.createElement("p");
+p1.textContent =
+  "저는 경성대학교에서 소프트웨어학을 전공하고있는 박상민 입니다.";
+body.appendChild(p1);
+
+let p2 = document.createElement("p");
+p2.textContent = "저는 3학년 수업이 시작되기전 유럽으로 여행을 갔다왔습니다.";
+body.appendChild(p2);
