0c3a531dd5d67e5c510712b565ce6f397f402e2c
SangMin Park <qkrtkdals268@naver.com>
2025-03-19 23:18:10 +0900

    3월 19일 제출

---
+++ 3월18일 과제/chapter_4/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Clock from "./chapter_04/Clock";
+function App() {
+  return (
+    <div>
+      <Clock />
+    </div>
+  );
+}
+export default App;
---
+++ 3월18일 과제/chapter_4/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+function tick() {
+  root.render(<App />);
+}
+setInterval(tick, 1000);
