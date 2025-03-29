c02781a492f1d9d1510de854aae8f65eb96f84f1
SangMin Park <qkrtkdals268@naver.com>
2025-03-29 23:34:53 +0900

    3월 29일 제출

---
+++ 3월25일 과제/comment_react/src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import CommentList from "./chapter_5/CommentList";
+function App() {
+  return (<CommentList />);
+}
+export default App;
---
+++ 3월25일 과제/comment_react/src/index.js
@@ -0,0 +1,12 @@
+// chapter_5
+//import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// const root = document.getElementById("root");
+// ReactDOM.createRoot(root).render(<App />);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import NotificationList from './chapter_6/NotificationList';
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<NotificationList />);
