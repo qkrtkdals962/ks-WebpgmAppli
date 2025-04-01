3ba864d0ac2d93178540741c10e8403f01eb47e2
SangMin Park <qkrtkdals268@naver.com>
2025-04-01 11:50:21 +0900

    AI로 자기소개 페이지 만들기

---
+++ 4월1일 과제/자기소개페이지AI/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>나의 자기소개</h1>
+      </header>
+
+      <section className="intro">
+        <h2>안녕하세요!</h2>
+        <p>저는 경성대학교 소프트웨어학과 3학년 21학번 박상민입니다.</p>
+        <p>
+          C언어와 웹 제작에 흥미를 가지고 있으며, 미래에는 게임 개발자와
+          웹 개발자가 되고 싶습니다.
+        </p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
