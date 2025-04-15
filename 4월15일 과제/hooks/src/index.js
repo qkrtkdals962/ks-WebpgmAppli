82c99cc072f8a44b17b4ac444ba9984e36258962
SangMin Park <qkrtkdals268@naver.com>
2025-04-15 17:43:07 +0900

    04월15일 제출

---
+++ 4월15일 과제/hooks/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from './use/ExpensiveCalculation';
+//import App from "./App.js";
+//import App from "./App2.js";
+const root = document.getElementById("root");
+ReactDOM.createRoot(root).render(<App />);
+//ReactDOM.createRoot(root).render(<App />);
+//ReactDOM.createRoot(root).render(<App2 />);
