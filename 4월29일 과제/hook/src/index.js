7c1659f72f6e7e324f217fd599c2fed92ab66928
SangMin Park <qkrtkdals268@naver.com>
2025-04-30 21:03:50 +0900

    0429수업

---
+++ 4월29일 과제/hook/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import InputComponent from './chapter_08/InputComponent';
+// import ButtonComponent from './chapter_08/ButtonComponent';
+// import Parent from './chapter_08/Parent';
+// import MyButton from './chapter_08/MyButton';
+// import ConfirmButton from './chapter_08/ConfirmButton';
+import LandingPage from './chapter_09/LandingPage';
+
+const root = document.getElementById("root");
+// ReactDOM.createRoot(root).render(<InputComponent />);
+// ReactDOM.createRoot(root).render(<ButtonComponent />);
+// ReactDOM.createRoot(root).render(<Parent />);
+// ReactDOM.createRoot(root).render(<MyButton />);
+// ReactDOM.createRoot(root).render(<ConfirmButton />);
+ReactDOM.createRoot(root).render(<LandingPage />);
